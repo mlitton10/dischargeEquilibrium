36dd7c34ba3836cb2759a0661b5e94e3d3b1597e
mlitton10 <mattlitton10@gmail.com>
2026-03-10 11:44:55 -0700

    Remove unused matplotlib import

--- atomic_collisional.py
+++ atomic_collisional.py
@@ -1,12 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
 from pycollisiondb.pycollisiondb import PyCollision
 from pathlib import Path
-
-plt.style.use('/home/matt/latex_and_matplotlib_styles/matplotlib_styles/physrev.mplstyle')  # Set full path to
-# physrev.mplstyle if the file is not in the same in directory as the notebook
-plt.rcParams['figure.dpi'] = "300"
 
 
 class AtomicData:
