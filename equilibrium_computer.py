1097a033faf08477a7ff23a7226484e3fd31a7eb
mlitton10 <mattlitton10@gmail.com>
2026-03-10 10:05:24 -0700

    Add imports to file

---
+++ equilibrium_computer.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from pycollisiondb.pycollisiondb import PyCollision
+
+plt.style.use('/home/matt/latex_and_matplotlib_styles/matplotlib_styles/physrev.mplstyle')  # Set full path to
+# physrev.mplstyle if the file is not in the same in directory as the notebook
+plt.rcParams['figure.dpi'] = "300"
+
+
