297b4fa3ae87a2e8cd86cb3152840443bfa1e54c
mlitton10 <mattlitton10@gmail.com>
2026-03-10 11:57:12 -0700

    remove .upper() call, pycollisiondb is case sensitive

--- atomic_collisional.py
+++ atomic_collisional.py
@@ -31,7 +31,7 @@ class AtomicDataHandler:
 
         """
 
-        self.species = gas_species.upper()
+        self.species = gas_species
         self.dataset = self.query_manager()
 
         pass
