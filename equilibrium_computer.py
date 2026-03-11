26996b52820566b10522a9b0fc61574dd536dd25
mlitton10 <mattlitton10@gmail.com>
2026-03-10 17:05:18 -0700

    add a bunch of methods to begin equilibrium modeling

--- equilibrium_computer.py
+++ equilibrium_computer.py
@@ -7,6 +7,57 @@ plt.style.use('/home/matt/latex_and_matplotlib_styles/matplotlib_styles/physrev.
 plt.rcParams['figure.dpi'] = "300"
 
 
+class Equilibrium:
+    def __init__(self, gas_species, gas_pressure_torr, length, radius):
+        self.species = gas_species
+        self.pressure = gas_pressure_torr
+        self.length = length
+        self.radius = radius
+        AtomicDataHandler(gas_species=test_species)
+        atomic_data = atomic_query.query_manager()
+
+        self.ion_mean_free_path = self.lambda_mean_free_path(self.pressure)
+
+
+    def lambda_mean_free_path(self, p):
+        """
+        Ion mean free path (heuristic)
+        :param p: neutral gas pressure, Torr
+        :return: mean free path, cm
+        """
+        return 1 / (330 * p)
+
+    def h_l(self, mfp, l):
+        """
+        Axial shape parameter, low pressure discharge < 100 mTorr; No field
+        :param mfp: mean free path, ion
+        :param l: length
+        :return: spaped axial length
+        """
+        return 0.86 / np.sqrt(3 + l / (2 * mfp))
+
+    def h_R(self, mfp, R):
+        """
+        radial shape parameter, low pressure discharge < 100 mTorr; No field
+        :param mfp: mean free path, ion
+        :param R: Radius
+        :return: shaped radius
+        """
+        return 0.8 / np.sqrt(4 + R / mfp)
+
+    def n_g(self, p):
+        """
+        heuristic for neutral density
+
+        :param p: neutral pressure in torr
+        :return: neutral density, cm^-3
+        """
+        return 330 * p / 1e-14
+
+    def compute_temperature(self):
+         C = self.atomic_data
+
+
 
 
 if __name__ == "__main__":
@@ -17,7 +68,7 @@ if __name__ == "__main__":
 
     f,a = plt.subplots(1,1)
 
-    a.plot(atomic_data.x_data, atomic_data.y_data, label=atomic_data.reaction)
+    a.plot(atomic_data.x_data_interpolated, atomic_data.y_data_interpolated, label=atomic_data.reaction)
 
     a.set_yscale('log')
     a.set_xscale('log')
