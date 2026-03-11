32d9f4372f5521c5d947f20e56cc388865c1debf
mlitton10 <mattlitton10@gmail.com>
2026-03-10 21:05:41 -0700

    All methods needed for computing discharge electron temp in eV for single species.

--- equilibrium_computer.py
+++ equilibrium_computer.py
@@ -6,6 +6,33 @@ plt.style.use('/home/matt/latex_and_matplotlib_styles/matplotlib_styles/physrev.
 # physrev.mplstyle if the file is not in the same in directory as the notebook
 plt.rcParams['figure.dpi'] = "300"
 
+m_e = 0.511e6 # ev/c^2
+M_p = 938e6 # ev/c^2
+
+atomic_mass_dict = {'H': 1,
+                    'D': 2,
+                    'T': 3,
+                    'He': 4,
+                    'C': 12,
+                    'Ne': 20.18,
+                    'Ar': 39.95}
+
+
+def find_roots(x, y):
+    # Detect sign changes: True where the sign of y[i] and y[i+1] are different
+    signs = np.sign(y)
+    sign_changes = np.abs(np.diff(signs)).astype(bool)
+
+    # Get the x and y values around the sign change
+    x1 = x[:-1][sign_changes]
+    x2 = x[1:][sign_changes]
+    y1 = y[:-1][sign_changes]
+    y2 = y[1:][sign_changes]
+
+    # Linear interpolation to find the exact x-value where y=0
+    # Formula derived from similar triangles: root = x1 + (x2 - x1) * |y1| / (|y1| + |y2|)
+    roots_x = x1 + (x2 - x1) * np.abs(y1) / (np.abs(y1) + np.abs(y2))
+    return roots_x
 
 class Equilibrium:
     def __init__(self, gas_species, gas_pressure_torr, length, radius):
@@ -13,10 +40,13 @@ class Equilibrium:
         self.pressure = gas_pressure_torr
         self.length = length
         self.radius = radius
-        AtomicDataHandler(gas_species=test_species)
-        atomic_data = atomic_query.query_manager()
+        self.neutral_density = self.n_g(self.pressure)
+        atomic_query = AtomicDataHandler(gas_species=test_species)
+        self.atomic_data = atomic_query.query_manager()
 
         self.ion_mean_free_path = self.lambda_mean_free_path(self.pressure)
+        self.M = atomic_mass_dict[self.species] * M_p
+        self.Te = self.compute_temperature()
 
 
     def lambda_mean_free_path(self, p):
@@ -55,7 +85,11 @@ class Equilibrium:
         return 330 * p / 1e-14
 
     def compute_temperature(self):
-         C = self.atomic_data
+        C = self.atomic_data.y_data_interpolated / np.sqrt(self.atomic_data.x_data_interpolated)
+        rhs = np.sqrt(self.M) * 2 * (self.h_l(self.ion_mean_free_path, self.length) / self.length + self.h_R(self.ion_mean_free_path, self.radius) / self.radius) * (1 / self.n_g(self.pressure))
+        roots = find_roots(self.atomic_data.x_data_interpolated, C - rhs)
+
+        return np.min(roots)
 
 
 
@@ -79,6 +113,16 @@ if __name__ == "__main__":
     a.set_xlabel('$R_{iz}$ ' + atomic_data.y_data_info['units'])
 
     plt.show()
+
+    CTI_Test_bed_parameters = {'gas_species': 'He',
+                               'gas_pressure_torr': 1e-4,
+                               'length': 100,
+                               'radius': 40
+    }
+
+    CTI_test_bed = Equilibrium('He', 1e-4, 100, 40)
+
+    print(CTI_test_bed.Te)
 
 
 
